ee116163ff70bedb2a1ea63b479ed64ffb5bba33
utqinadhif <nadhifbeo@gmail.com>
2024-01-11 14:02:29 +0700

    try

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,22 @@
-export default function Page(): JSX.Element {
+import Image from 'next/image'
+
+export default async function Page() {
+    const apiF = await fetch('http://localhost:2000/session/add', {
+        cache: 'no-store',
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: 'cobaah' + Math.floor(Date.now() / 1000)
+        }),
+    })
+    const res = await apiF.json()
+
     return (
         <main>
-            joss
+            <Image src={res.data.qr} alt="joss" width={400} height={400}></Image>
         </main>
     );
 }
